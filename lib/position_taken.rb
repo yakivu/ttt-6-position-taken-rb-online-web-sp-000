4e83887ba2466a73a7e45d83e5ca7de7426b133b
Adam Walker <adamw207@yahoo.com>
2019-12-12 00:30:10 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,43 +1,38 @@
 # code your #position_taken? method here!
 
 def
-describe '#position_taken? in lib/position_taken.rb' do
-  it 'accepts a board and the index to check as arguments' do
+
     board = []
     index = 0
 
     
   end
 def
-  it 'returns false if the board has an empty string " " in the desired index' do
+ 
     board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
     index = 0
 
-    expect(position_taken?(board, index)).to be(false)
+    
   end
 def
-   it 'returns false if the board has an empty string "" in the index' do
+   
     board = ["", " ", " ", " ", " ", " ", " ", " ", " "]
     index = 0
 
-    expect(position_taken?(board, index)).to be(false)
+    
   end
 def
-  it 'returns false if the board has nil in the index' do
+
     board = [nil, " ", " ", " ", " ", " ", " ", " ", " "]
     index = 0
 
-    expect(position_taken?(board, index)).to be(false)
+    
 
     index = 1
-    expect(position_taken?(board, index)).to be(false)
+  
   end
 def
-  it 'returns true if the board has a value of "X" or "O" in the index' do
-    board = ["X", " ", " ", " ", " ", " ", " ", " ", "O"]
-
-    index = 0
-    expect(position_taken?(board, index)).to be(true)
+  
 
     index = 8
     expect(position_taken?(board, index)).to be(true)
